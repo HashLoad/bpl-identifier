11551c86d406090d1ad2c14572aac537fa8a4a68
Rodrigo Bernardi <coloradonanet@unochapeco.edu.br>
2019-05-30 20:06:40 -0300

    Added return by exit code

--- BplIdentifier.dpr
+++ BplIdentifier.dpr
@@ -15,14 +15,22 @@ begin
   LBpl := ParamStr(1);
   try
     LHnd := LoadPackage(LBpl);
-    GetPackageInfo(LHnd, nil, LFlag, PackageInfoProc);
+    GetPackageInfo(LHnd, nil, LFlag, nil);
     UnloadPackage(LHnd);
   except
     ExitCode := 1;
   end;
 
   if not(LFlag and pfRunOnly = pfRunOnly) then
-    WriteLn('Design time: ' + LBpl)
+  begin
+    WriteLn('Design time: ' + LBpl);
+    ExitCode := 0;
+  end
   else
+  begin
     WriteLn('Run time: ' + LBpl);
+    ExitCode := 1;
+  end;
+
+
 end.
